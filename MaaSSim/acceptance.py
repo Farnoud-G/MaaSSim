7439bcd21492e987658714d7cf9982abbceef7b7
FarnoudGhasemi <farnoud_g@yahoo.com>
2022-04-23 02:07:07 +0200

    kpi functions created

--- MaaSSim/acceptance.py
+++ MaaSSim/acceptance.py
@@ -1,8 +1,105 @@
+from .traveller import travellerEvent
+from .driver import driverEvent
 import pandas as pd
 import numpy as np
 from dotmap import DotMap
 import math
 import random as random
+
+def RA_kpi_veh(*args,**kwargs):
+
+    sim =  kwargs.get('sim', None)
+    params = sim.params
+    run_id = kwargs.get('run_id', None)
+    simrun = sim.runs[run_id]
+    vehindex = sim.inData.vehicles.index
+    df = simrun['rides'].copy()  # results of previous simulation
+    DECIDES_NOT_TO_DRIVE = df[df.event == driverEvent.DECIDES_NOT_TO_DRIVE.name].veh  # track drivers out
+    dfs = df.shift(-1)  # to map time periods between events
+    dfs.columns = [_ + "_s" for _ in df.columns]  # columns with _s are shifted
+    df = pd.concat([df, dfs], axis=1)  # now we have time periods
+    df = df[df.veh == df.veh_s]  # filter for the same vehicles only
+    df = df[~(df.t == df.t_s)]  # filter for positive time periods only
+    df['dt'] = df.t_s - df.t  # make time intervals
+    ret = df.groupby(['veh', 'event_s'])['dt'].sum().unstack()  # aggreagted by vehicle and event
+    ret.columns.name = None
+    ret = ret.reindex(vehindex)  # update for vehicles with no record
+    ret['nRIDES'] = df[df.event == driverEvent.ARRIVES_AT_DROPOFF.name].groupby(['veh']).size().reindex(ret.index)
+    ret['nREJECTS'] = df[df.event==driverEvent.REJECTS_REQUEST.name].groupby(['veh']).size().reindex(ret.index)
+    for status in driverEvent:
+        if status.name not in ret.columns:
+            ret[status.name] = 0
+    DECIDES_NOT_TO_DRIVE.index = DECIDES_NOT_TO_DRIVE.values
+    ret['OUT'] = DECIDES_NOT_TO_DRIVE
+    ret['OUT'] = ~ret['OUT'].isnull()
+    ret['DRIVING_TIME'] = ret.ARRIVES_AT_PICKUP + ret.ARRIVES_AT_DROPOFF #we assum there is no repositioning
+    ret['DRIVING_DIST'] = ret['DRIVING_TIME']*(params.speeds.ride/1000)   #here we assume the speed is constant on the network
+    ret['IDLE_TIME'] = ret['ENDS_SHIFT'] + ret['RECEIVES_REQUEST']
+    ret.fillna(0, inplace=True)  
+    d = df[df['event_s']=='ARRIVES_AT_DROPOFF']
+    if len(d) != 0:
+        d['REVENUE'] = d.apply(lambda row: max(row['dt'] * (params.speeds.ride/1000) * params.platforms.fare + params.platforms.base_fare, params.platforms.min_fare), axis=1)*(1-params.platforms.comm_rate)
+        ret['REVENUE'] = d.groupby(['veh']).sum().REVENUE
+    else:
+        ret['REVENUE'] = 0
+    ret['COST'] = ret['DRIVING_DIST'] * (params.d2d.fuel_cost) # Operating Cost (OC)
+    ret['PROFIT'] = ret['REVENUE'] - ret['COST']
+    ret['ACCEPTANCE_RATE'] = (ret['nRIDES']-ret['nREJECTS'])/ret['nRIDES']*100
+    ret.replace([np.inf, -np.inf], np.nan, inplace=True)
+    ret.fillna(0, inplace=True)  
+
+    ret = ret[['ACCEPTANCE_RATE','PROFIT','IDLE_TIME','nRIDES','nREJECTS','DRIVING_TIME','DRIVING_DIST','REVENUE','COST']]#+ [_.name for _ in driverEvent]]
+    ret.index.name = 'veh'
+    
+    # KPIs
+    kpi = ret.agg(['sum', 'mean', 'std'])
+    kpi['nV'] = ret.shape[0]
+    return {'veh_exp': ret, 'veh_kpi': kpi}
+
+
+def RA_kpi_pax(*args,**kwargs):
+
+    sim = kwargs.get('sim', None)
+    params = sim.params
+    run_id = kwargs.get('run_id', None)
+    simrun = sim.runs[run_id]
+    paxindex = sim.inData.passengers.index
+    df = simrun['trips'].copy()  # results of previous simulation
+    PREFERS_OTHER_SERVICE = df[df.event == travellerEvent.PREFERS_OTHER_SERVICE.name].pax  # track drivers out
+    dfs = df.shift(-1)  # to map time periods between events
+    dfs.columns = [_ + "_s" for _ in df.columns]  # columns with _s are shifted
+    df = pd.concat([df, dfs], axis=1)  # now we have time periods
+    df = df[df.pax == df.pax_s]  # filter for the same vehicles only
+    df = df[~(df.t == df.t_s)]  # filter for positive time periods only
+    df['dt'] = df.t_s - df.t  # make time intervals
+    ret = df.groupby(['pax', 'event_s'])['dt'].sum().unstack()  # aggreagted by vehicle and event
+
+    ret.columns.name = None
+    ret = ret.reindex(paxindex)  # update for pax with no record
+
+    ret.index.name = 'pax'
+    ret = ret.fillna(0)
+
+    for status in travellerEvent:
+        if status.name not in ret.columns:
+            ret[status.name] = 0  # cover all statuses
+    PREFERS_OTHER_SERVICE.index = PREFERS_OTHER_SERVICE.values
+    ret['OUT'] = PREFERS_OTHER_SERVICE
+    ret['OUT'] = ~ret['OUT'].isnull()
+    ret['LOST_PATIENCE'] = ret.apply(lambda row: False if row['ARRIVES_AT_DROPOFF']>0 else True,axis=1) 
+    ret['TRAVEL'] = ret['ARRIVES_AT_DROPOFF']  # time with traveller (paid time)
+    ret['WAIT_TIME'] = (ret['RECEIVES_OFFER'] + ret['MEETS_DRIVER_AT_PICKUP'] + ret.get('LOSES_PATIENCE', 0))/60  #in minute
+    ret.fillna(0, inplace=True)
+
+    ret = ret[['WAIT_TIME','TRAVEL','LOST_PATIENCE']] #+ [_.name for _ in travellerEvent]]
+    ret.index.name = 'pax'
+
+    kpi = ret.agg(['sum', 'mean', 'std'])
+    kpi['nP'] = ret.shape[0]
+    return {'pax_exp': ret, 'pax_kpi': kpi}
+
+
+
 
 def f_decline(veh, **kwargs):
     
