b9f29864574c64acb71036d52b8701ed51dd6848
FarnoudGhasemi <farnoud_g@yahoo.com>
2022-04-27 19:00:00 +0200

    27/04/2022

--- MaaSSim/day_to_day.py
+++ MaaSSim/day_to_day.py
@@ -20,7 +20,7 @@ def driver_opt_out(veh, **kwargs): # user defined function to represent agent pa
     
     if params.d2d.probabilistic:
         working_P = (math.exp(working_U))/(math.exp(working_U) + math.exp(not_working_U))
-        sim.driver_p.append(working_P)
+        #sim.driver_p.append(working_P)
         return bool(working_P < random.uniform(0,1))
     else:
         return bool(working_U < not_working_U)
@@ -42,7 +42,7 @@ def traveller_opt_out(pax, **kwargs):
    
 
      #if True :
-#         print('rhmoney',-params.d2d.get('B_fare',1)*rh_fare,'----','time', - params.d2d.get('B_time',0.1)*(req.ttrav.total_seconds()/60+exp_wait_t) + pax.pax.get('exp_utility_eps', 0))
+#         print('rhmoney',-params.d2d.get('B_fare',1)*rh_fare,'----','time', - params.d2d.get('B_time',0.1)* (req.ttrav.total_seconds()/60+exp_wait_t))
 #         print('altmoney',-params.d2d.get('B_fare',1)*params.PT_fare*req.dist/1000,'----','time', -params.d2d.get('B_time',0.1)*(req.dist/params.PT_speed)/60)
 
 #         print('rh_U= ',rh_U,'---', 'alt_U= ', alt_U)
@@ -92,10 +92,12 @@ def d2d_kpi_veh(*args,**kwargs):
     
     d = df[df['event_s']=='ARRIVES_AT_DROPOFF']
     if len(d) != 0:
-        d['REVENUE'] = d.apply(lambda row: max(row['dt'] * (params.speeds.ride/1000) * params.platforms.fare + params.platforms.base_fare, params.platforms.min_fare), axis=1)*(1-params.platforms.comm_rate)
-        ret['REVENUE'] = d.groupby(['veh']).sum().REVENUE
+        d['TRIP_FARE'] = d.apply(lambda row: max(row['dt'] * (params.speeds.ride/1000) * params.platforms.fare + params.platforms.base_fare, params.platforms.min_fare), axis=1)
+        ret['TRIP_FARE'] = d.groupby(['veh']).sum().TRIP_FARE
     else:
-        ret['REVENUE'] = 0
+        ret['TRIP_FARE'] = 0
+    ret['REVENUE'] = ret['TRIP_FARE']*(1-params.platforms.comm_rate)
+    ret['COMMISSION'] = ret['TRIP_FARE']*params.platforms.comm_rate
     ret['COST'] = ret['DRIVING_DIST'] * (params.d2d.fuel_cost) # Operating Cost (OC)
     ret['PROFIT'] = ret['REVENUE'] - ret['COST']
     ret['mu'] = ret.apply(lambda row: 1 if row['OUT'] == False else 0, axis=1)
@@ -108,22 +110,21 @@ def d2d_kpi_veh(*args,**kwargs):
     #update_learning_status(sim, ret)
     #---------------------------------------------------------
     # Djavadian & Chow (2017)
-    # expectation at the beginning of day
-    # pre_exp_inc = params.d2d.ini_exp_income if run_id == 0 else sim.res[run_id-1].veh_exp.EXPECTED_INC
-    # ave_income = 0 if ret.mu.sum() == 0 else ret.ACTUAL_INC.sum()/ret.mu.sum()
-    # # created expection at the end of day
-    # ret['EXPECTED_INC'] = (1-params.d2d.omega)*pre_exp_inc + params.d2d.omega*ret.mu*ret.ACTUAL_INC+ \
-    #                        params.d2d.omega*(1-ret.mu)*ave_income
+    pre_exp_inc = params.d2d.ini_exp_income if run_id == 0 else sim.res[run_id-1].veh_exp.EXPECTED_INC
+    ave_income = 0 if ret.mu.sum() == 0 else ret.ACTUAL_INC.sum()/ret.mu.sum()
+    ret['EXPECTED_INC'] = (1-params.d2d.omega)*pre_exp_inc + params.d2d.omega*ret.mu*ret.ACTUAL_INC+ \
+                           params.d2d.omega*(1-ret.mu)*ave_income
     #---------------------------------------------------------
     # Arjan (2021)
-    ret['pre_exp_inc'] = params.d2d.ini_exp_income if run_id == 0 else sim.res[run_id-1].veh_exp.EXPECTED_INC
-    ret['EXPECTED_INC'] = ret.apply(lambda row: row['pre_exp_inc'] if row['mu']==0 or sim.vehs[row.name].veh.get('learning','on')=='off' else (1-(row['nDAYS_WORKED']+1)**(-(params.d2d.kappa)))*row['pre_exp_inc'] + ((row['nDAYS_WORKED']+1)**(-(params.d2d.kappa)))*row['ACTUAL_INC'], axis=1)
+    # ret['pre_exp_inc'] = params.d2d.ini_exp_income if run_id == 0 else sim.res[run_id-1].veh_exp.EXPECTED_INC
+    # ret['EXPECTED_INC'] = ret.apply(lambda row: row['pre_exp_inc'] if row['mu']==0 or sim.vehs[row.name].veh.get('learning','on')=='off' else (1-(row['nDAYS_WORKED']+1)**(-(params.d2d.kappa)))*row['pre_exp_inc'] + ((row['nDAYS_WORKED']+1)**(-(params.d2d.kappa)))*row['ACTUAL_INC'], axis=1)
     #---------------------------------------------------------
     # Nejc model
     
     
 
-    ret = ret[['nRIDES','nREJECTED', 'nDAYS_WORKED', 'DRIVING_TIME', 'DRIVING_DIST', 'REVENUE', 'COST','ACTUAL_INC', 'EXPECTED_INC', 'OUT','mu'] + [_.name for _ in driverEvent]]
+    ret = ret[['nRIDES','nREJECTED', 'nDAYS_WORKED', 'DRIVING_TIME', 'DRIVING_DIST', 'REVENUE', 'COST','COMMISSION',
+               'TRIP_FARE','ACTUAL_INC', 'EXPECTED_INC', 'OUT','mu'] + [_.name for _ in driverEvent]]
     ret.index.name = 'veh'
     
     # KPIs
@@ -195,8 +196,14 @@ def d2d_kpi_pax(*args,**kwargs):
     ret.fillna(0, inplace=True)
     
     # Traveller adaptation (learning) --------------------------------------------------------------------------------- #
-    ret['pre_exp_wt'] = params.d2d.ini_exp_wt if run_id == 0 else sim.res[run_id-1].pax_exp.EXPECTED_WT
-    ret['EXPECTED_WT'] = ret.apply(lambda row: row['pre_exp_wt'] if row['mu']==0 or sim.pax[row.name].pax.get('learning','on')=='off' else (1-(row['nDAYS_HAILED']+1)**(-(params.d2d.kappa)))*row['pre_exp_wt'] + ((row['nDAYS_HAILED']+1)**(-(params.d2d.kappa)))*row['ACTUAL_WT'], axis=1)                                    
+    # Djavadian-------------------------------------------------------------------------------------------------------- #
+    pre_exp_wt = params.d2d.ini_exp_wt if run_id == 0 else sim.res[run_id-1].pax_exp.EXPECTED_WT
+    ave_wt = 0 if ret.mu.sum() == 0 else ret.ACTUAL_WT.sum()/ret.mu.sum()
+    ret['EXPECTED_WT'] = (1-params.d2d.omega)*pre_exp_wt + params.d2d.omega*ret.mu*ret.ACTUAL_WT+ \
+                           params.d2d.omega*(1-ret.mu)*ave_wt
+    # Arjan------------------------------------------------------------------------------------------------------------ #
+    # ret['pre_exp_wt'] = params.d2d.ini_exp_wt if run_id == 0 else sim.res[run_id-1].pax_exp.EXPECTED_WT
+    # ret['EXPECTED_WT'] = ret.apply(lambda row: row['pre_exp_wt'] if row['mu']==0 or sim.pax[row.name].pax.get('learning','on')=='off' else (1-(row['nDAYS_HAILED']+1)**(-(params.d2d.kappa)))*row['pre_exp_wt'] + ((row['nDAYS_HAILED']+1)**(-(params.d2d.kappa)))*row['ACTUAL_WT'], axis=1)                                    
     # ----------------------------------------------------------------------------------------------------------------- #
 
     ret = ret[['ACTUAL_WT', 'EXPECTED_WT', 'OUT','mu','nDAYS_HAILED'] + [_.name for _ in travellerEvent]]
