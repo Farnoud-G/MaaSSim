cb1bf3fc793976ed4e4de9ed7501c17543922891
FarnoudGhasemi <farnoud_g@yahoo.com>
2023-10-23 11:56:45 +0200

    10/23/2023

--- MaaSSim/simulators.py
+++ MaaSSim/simulators.py
@@ -127,17 +127,16 @@ def simulate(config="data/config.json", inData=None, params=None, **kwargs):
         nV = 0 if day == 0 else sim.res[day-1].veh_exp.OUT.value_counts().get(False, 0)
         print('nP = ', nP, '   nV = ',nV)
 
-        platfrom_profit = sim.res[day - 1].pax_kpi.plat_revenue['sum'] if len(sim.res) > 0 else 0  # - marketing cost
         # Strategy============================================================
 
         # 1- Trip fare adjustment -------------------------------------------
         # sim.platforms.fare = params.platforms.fare
 
         # 2- Commission rate adjustment -------------------------------------
-        if 300 == day:
-            # sim.platforms.fare[1] = 2 #euro/km
-            sim.platforms.comm_rate[1] = 0.50
-            print('Tragedy STARTS!')
+        # if 300 == day:
+        #     # sim.platforms.fare[1] = 2 #euro/km
+        #     sim.platforms.comm_rate[1] = 0.50
+        #     print('Tragedy STARTS!')
 
         # 3- Discount adjustment -------------------------------------------
         # params.platforms.discount = 0.20 if 300<=day<350 else 0
@@ -147,7 +146,7 @@ def simulate(config="data/config.json", inData=None, params=None, **kwargs):
         #     params.platforms.discount = 0
 
         # 4- Marketing adjustment ------------------------------------------
-        sim.platforms.daily_marketing[1] = True if len(sim.res) in range(0, 100) else False
+        sim.platforms.daily_marketing[1] = True if len(sim.res) in range(0, 50) else False
 
         # ====================================================================
 
